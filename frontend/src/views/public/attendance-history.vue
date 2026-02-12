91cb8e5e542a7ffd5669592e3732c84735a905b6
vanhdeventr1 <dinaramadani121104@gmail.com>
2026-02-12 09:13:33 +0700

    added template for attendance and attendace history views

---
+++ frontend/src/views/public/attendance-history.vue
@@ -0,0 +1,55 @@
+<template>
+  <SidebarLayout>
+    <div class="flex flex-col gap-y-6 animate-in">
+      <div class="bg-white border border-[#8C352D] rounded-2xl p-6 shadow-sm">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+          <div
+            class="bg-[#FFF0EE] border border-[#8C352D] p-6 rounded-2xl shadow-sm"
+          >
+            <p class="text-3xl font-bold text-[#8C352D]">5</p>
+            <p class="text-[#8C352D] font-medium">Absen Minggu Ini</p>
+          </div>
+
+          <div
+            class="bg-[#FFF0EE] border border-[#8C352D] p-6 rounded-2xl shadow-sm"
+          >
+            <p class="text-3xl font-bold text-[#8C352D]">2</p>
+            <p class="text-[#8C352D] font-medium">Terlambat</p>
+          </div>
+
+          <div
+            class="bg-[#FFF0EE] border border-[#8C352D] p-6 rounded-2xl shadow-sm"
+          >
+            <p class="text-3xl font-bold text-[#8C352D]">1</p>
+            <p class="text-[#8C352D] font-medium">Sakit</p>
+          </div>
+          <div
+            class="bg-[#FFF0EE] border border-[#8C352D] p-6 rounded-2xl shadow-sm"
+          >
+            <p class="text-3xl font-bold text-[#8C352D]">0</p>
+            <p class="text-[#8C352D] font-medium">Menunggu Persetujuan</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </SidebarLayout>
+</template>
+
+<script setup lang="ts">
+import SidebarLayout from "@/layout/sidebarpublic.vue";
+</script>
+<style>
+.animate-in {
+  animation: fadeIn 0.3s ease-out;
+}
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
---
+++ frontend/src/views/public/attendance.vue
@@ -0,0 +1,22 @@
+<template>
+  <SidebarLayout></SidebarLayout>
+</template>
+
+<script setup lang="ts">
+import SidebarLayout from "@/layout/sidebarpublic.vue";
+</script>
+<style>
+.animate-in {
+  animation: fadeIn 0.3s ease-out;
+}
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
