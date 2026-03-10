b10ce3c42ba07aa4f89443066b7dc4f92b0d6c61
vanhdeventr1 <dinaramadani121104@gmail.com>
2026-03-10 10:24:31 +0700

    modified entities for CRUD Attendance

--- backend/src/features/attendance/entities/attendance.entity.ts
+++ backend/src/features/attendance/entities/attendance.entity.ts
@@ -3,11 +3,15 @@ import {
   Column,
   DataType,
   ForeignKey,
+  HasMany,
   Model,
   PrimaryKey,
   Table,
 } from "sequelize-typescript";
+import { ResizeOption } from "src/cores/helpers/sharp.helper";
 import type { TypeWrapper } from "src/cores/helpers/type-wrapper";
+import { AttendanceImage } from "src/features/attendance-image/entities/attendance-image.entity";
+import { AttendanceSetting } from "src/features/attendance-setting/entities/attendance-setting.entity";
 import { Permit } from "src/features/permit/entities/permit.entity";
 import { User } from "src/features/user/entities/user.entity";
 
@@ -35,8 +39,8 @@ export class Attendance extends Model {
   @Column({ type: DataType.DATE, allowNull: true })
   clock_in: Date;
 
-  @Column({ type: DataType.DATE, allowNull: false })
-  shift_time: Date;
+  @Column({ type: DataType.DATE, allowNull: true })
+  clock_out: Date;
 
   @Column({ type: DataType.BOOLEAN, allowNull: false, defaultValue: false })
   is_late: boolean;
@@ -53,10 +57,47 @@ export class Attendance extends Model {
   @Column({ type: DataType.TEXT, allowNull: true })
   note: string;
 
+  @ForeignKey(() => AttendanceSetting)
+  @Column({ type: DataType.BIGINT, allowNull: true })
+  attendance_setting_id: number;
+
+  @BelongsTo(() => AttendanceSetting, { foreignKey: "attendance_setting_id" })
+  attendance_setting: TypeWrapper<AttendanceSetting>;
+
   @ForeignKey(() => Permit)
   @Column({ type: DataType.BIGINT, allowNull: true })
   permit_id: number;
 
   @BelongsTo(() => Permit, { foreignKey: "permit_id" })
   permit: TypeWrapper<Permit>;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.BIGINT, allowNull: false })
+  created_by: number;
+
+  @BelongsTo(() => User, {
+    foreignKey: "created_by",
+  })
+  created_by_user: TypeWrapper<User>;
+
+  @HasMany(() => AttendanceImage)
+  attendance_images: TypeWrapper<AttendanceImage[]>;
+
+  static imageDimension: { attendanceImage: ResizeOption } = {
+    attendanceImage: {
+      dimensions: [
+        {
+          width: 100,
+          fit: "inside",
+          prefix: "100",
+        },
+        {
+          width: 500,
+          fit: "inside",
+          prefix: "500",
+        },
+      ],
+      path: "attendance/images",
+    },
+  };
 }
