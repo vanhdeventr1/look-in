670f9ed8806a09d0f510b08df808bf7e722e3567
vanhdeventr1 <dinaramadani121104@gmail.com>
2026-03-10 10:24:07 +0700

    added DTO for CRUD attendance

--- backend/src/features/attendance/dto/create-attendance.dto.ts
+++ backend/src/features/attendance/dto/create-attendance.dto.ts
@@ -1,9 +1,11 @@
 export class CreateAttendanceDto {
-  clock_in: Date;
-  shift_time: Date;
-  is_late: boolean;
-  late_duration: number;
-  gps_lat: string;
-  gps_lng: string;
-  note: string;
+  clock_in?: Date;
+  clock_out?: Date;
+  is_late?: boolean;
+  late_duration?: number;
+  gps_lat?: string;
+  gps_lng?: string;
+  note?: string;
+  attendance_setting_id?: number;
+  permit_id?: number;
 }
--- backend/src/features/attendance/dto/update-attendance.dto.ts
+++ backend/src/features/attendance/dto/update-attendance.dto.ts
@@ -1,3 +1,4 @@
-export class UpdateAttendanceDto {
-  note: string;
-}
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateAttendanceDto } from "./create-attendance.dto";
+
+export class UpdateAttendanceDto extends PartialType(CreateAttendanceDto) {}
