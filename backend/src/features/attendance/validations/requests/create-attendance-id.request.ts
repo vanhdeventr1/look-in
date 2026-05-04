4c57bb6548d9d4329d45afc666386bbbeff188ea
vanhdeventr1 <dinaramadani121104@gmail.com>
2026-05-04 14:24:56 +0700

    removed permit id

--- backend/src/features/attendance/validations/requests/create-attendance-id.request.ts
+++ backend/src/features/attendance/validations/requests/create-attendance-id.request.ts
@@ -1,9 +1,7 @@
 import * as Joi from "joi";
 
 export const createAttendanceSchema = Joi.object({
-  clock_out: Joi.date().optional().allow(null),
   gps_lat: Joi.string().required(),
   gps_lng: Joi.string().required(),
-  permit_id: Joi.number().integer().optional().allow(null),
   note: Joi.string().optional().allow(null, ""),
 });
